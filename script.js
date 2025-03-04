ce363ed3e87f26c1b0b876391318a53f9bd97de1
mbilal35 <mbilalseo9@gmail.com>
2025-03-04 14:18:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ async function generateMeme() {
 
        // Use Imgflip API for meme generation
        const templateId = '181913649'; // Drake Hotline Bling template
-       const username = 'YOUR_IMGFLIP_USERNAME'; // Replace with your Imgflip username
-       const password = 'YOUR_IMGFLIP_PASSWORD'; // Replace with your Imgflip password
+       const username = 'bilal35'; // Replace with your Imgflip username
+       const password = '456@gmail'; // Replace with your Imgflip password
 
        const response = await fetch(https://api.imgflip.com/caption_image?template_id=${templateId}&username=${username}&password=${password}&text0=${prompt});
        const data = await response.json();
